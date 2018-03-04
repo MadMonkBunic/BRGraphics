7da69bfe401d10d29e830b2cf3f7b69cd1132ad3
Eddy Leon <xltunited@gmail.com>
2018-03-04 16:55:37 -0500

    ty.html page horizontal scrolling

--- js/port.js
+++ js/port.js
@@ -8,6 +8,21 @@ $grid.imagesLoaded().progress( function() {
   $grid.masonry('layout');
 });
 
+function scrollHorizontally(e) {
+  e = window.event || e;
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  document.getElementById('scrolling-div').scrollLeft -= (delta*120); // Multiplied by 40
+  e.preventDefault();
+}
+if (document.getElementById('scrolling-div').addEventListener) {
+  // IE9, Chrome, Safari, Opera
+  document.getElementById('scrolling-div').addEventListener("mousewheel", scrollHorizontally, false);
+  // Firefox
+  document.getElementById('scrolling-div').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+  // IE 6/7/8
+  document.getElementById('scrolling-div').attachEvent("onmousewheel", scrollHorizontally);
+}
 
 //var $grid = $('.grid').imagesLoaded( function() {
 //  // init Masonry after all images have loaded
